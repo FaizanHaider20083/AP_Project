864c01ab787e5d85544e8591846fda49539d19c1
Faizan Haider <faizan20083@iiitd.ac.in>
2021-12-04 03:47:57 +0530

    menu

--- Will_Hero/src/sample/Main.java
+++ Will_Hero/src/sample/Main.java
@@ -10,14 +10,16 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("Untitled.fxml"));
+        primaryStage.setTitle("Will Hero Online");
+        primaryStage.setScene(new Scene(root, 793, 373));
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
+        System.out.println("Hello world");
         launch(args);
+
     }
 }
