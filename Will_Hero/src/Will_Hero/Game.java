f03a1e4ea7660312c2d11863161980bc5f644354
Divyam Gupta <82924774+fruitkijin@users.noreply.github.com>
2021-12-04 15:27:29 +0530

    Faizan Re-Commit

--- Will_Hero/src/Will_Hero/Game.java
+++ Will_Hero/src/Will_Hero/Game.java
@@ -1,8 +1,28 @@
 package Will_Hero;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Game{
+public class Game extends Application {
     private int id;
     Game(int id){
         this.id = id;
     }
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("Untitled.fxml"));
+        primaryStage.setTitle("Will Hero Online");
+        primaryStage.setScene(new Scene(root, 793, 373));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        launch(args);
+
+    }
 }
+
