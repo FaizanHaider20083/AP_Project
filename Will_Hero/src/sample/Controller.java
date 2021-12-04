596104bec340117b8cb72d9d980b75ca85e56387
Faizan Haider <faizan20083@iiitd.ac.in>
2021-12-04 20:12:07 +0530

    Loading screen scratch

--- Will_Hero/src/sample/Controller.java
+++ Will_Hero/src/sample/Controller.java
@@ -2,8 +2,18 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Controller {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
     @FXML
     void play(ActionEvent e){
         System.out.println("Play ");
@@ -16,8 +26,17 @@ public class Controller {
         System.exit(1);
     }
     @FXML
-    void load(ActionEvent e){
-        System.out.println("Load");
-        System.exit(1);
+    void load(ActionEvent e) throws IOException {
+        try {
+            System.out.println("Load");
+            Parent root = FXMLLoader.load(getClass().getResource("Scene2.fxml"));
+            stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch (Exception f){
+            f.printStackTrace();
+        }
     }
 }
